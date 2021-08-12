745d921cbf8fa63788dc2c19e936caabf8ff194c
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-08-12 23:11:13 +0600

    event listener and anonymous function added

--- js/event.js
+++ js/event.js
@@ -11,3 +11,27 @@ blue.onclick = colorBlue;
 function colorBlue() {
   document.body.style.backgroundColor = "blue";
 }
+
+ // annonymus function
+       const greenButton =  document.getElementById('button-green');
+       greenButton.onclick = function (){
+           document.body.style.backgroundColor = 'green';
+}
+       
+ // using addEventListener
+       const goldenRod = document.getElementById('button-golden-rod');
+       goldenRod.addEventListener('click', colorGoldenRod)
+       function colorGoldenRod(){
+           document.body.style.backgroundColor = 'goldenrod';
+}
+       
+ // addEventListener with anonymous function
+       const hotPink = document.getElementById('button-hot-pink');
+       hotPink.addEventListener('click', function (){
+           document.body.style.backgroundColor = 'hotpink';
+       })
+
+  // Direct code
+       document.getElementById('button-light-blue').addEventListener('click', function (){
+           document.body.style.backgroundColor = 'lightblue';
+       })
